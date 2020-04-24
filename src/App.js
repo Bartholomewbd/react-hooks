46545b2eae6278e68cd6906b4db743f3ee9946b0
Brian Bartholomew <bartholomewbd@gmail.com>
2020-04-24 09:16:34 -0400

    setting that state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
+
+//problem is you cannot have multiple hooks :-/
+
+
+let value
+
+function useState(initialState) {
+  if (typeof value === 'undefined') {
+    value = initialState
+  }
+
+  function setState(nextValue) {
+    console.log(nextValue)
+    value = nextValue
+    ReactDOM.render(<MyName />, document.getElementById('root'))
+  }
+  console.log(value)
+  return [value, setState]
+}
 
 const MyName = () => {
   const [name, setName] = useState('')
